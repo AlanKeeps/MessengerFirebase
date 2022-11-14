d2a3f8f8c59097ef66aa9587f8bcbb723c2c634e
AlanKeeps <alan.keeps.walking@gmail.com>
2022-11-14 18:06:36 +0300

    create chatActivity

---
+++ app/src/main/java/com/example/messenger/ChatActivity.java
@@ -0,0 +1,35 @@
+package com.example.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private TextView textViewTitle;
+    private View OnlineStatus;
+    private RecyclerView recyclerViewMessages;
+    private EditText editTextMessage;
+    private ImageView imageViewSendMessage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        initViews();
+    }
+
+    private void initViews() {
+        textViewTitle = findViewById(R.id.textViewTitle);
+        OnlineStatus = findViewById(R.id.OnlineStatus);
+        recyclerViewMessages = findViewById(R.id.recyclerViewMessages);
+        editTextMessage = findViewById(R.id.editTextMessage);
+        imageViewSendMessage = findViewById(R.id.imageViewSendMessage);
+    }
+}
